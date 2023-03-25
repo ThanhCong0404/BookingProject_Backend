f80f201d9747b7cbb4473a67e31738c84bee16bd
ThanhCong0404 <Thanhcong04042001@gmail.com>
2023-03-25 16:42:06 +0700

    create simple hotel CRUD API and simple handler exception throught next()middleware

--- index.js
+++ index.js
@@ -26,10 +26,26 @@ mongoose.connection.on("disconnected",()=> {
 })
 
 //midlewares
+
+app.use(express.json());
+
 app.use("/api/auth",authRoute);
 app.use("/api/users",usersRoute);
 app.use("/api/hotels",hotelsRoute);
 app.use("/api/rooms",roomsRoute);
+
+
+app.use((err,req,res,next)=>{
+    const errorStatus = err.status || 500;
+    const errMessage = err.message || "Something went wrong !";
+
+    return res.status(errorStatus).json({
+        success:false,
+        status: errorStatus,
+        message: errMessage,
+        stack: err.stack
+    });
+})
 
 app.listen(8888,()=>{
     connect();
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,6 +1,77 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
 
 var router= express.Router();
+
+//create
+router.post("/", async (req,res) => {
+    const newHotel = new Hotel(req.body);
+    
+    try {
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
+        
+        
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+//update
+router.put("/:id", async (req,res) => {
+    
+
+    try {
+        const updateHotel = await Hotel.findByIdAndUpdate(req.params.id,{ $set: req.body}); //add more option {new:true} if database dont change
+        res.status(200).json(updateHotel);
+        
+        
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+//delete
+router.delete("/:id", async (req,res) => {
+    
+
+    try {
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has been deleted.");
+        
+        
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+//Get
+router.get("/:id", async (req,res) => {
+    
+
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel);
+        
+        
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+
+//Get All
+router.get("/", async (req,res,next) => {    
+
+    try {
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+        
+        
+    } catch (error) {
+        next(error);
+    }
+})
 
 
 
